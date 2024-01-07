a50b5c830ccf2fb0f4eff51118435edb8021975e
Rodrigo Tutz <rodrigoantunestutz@gmail.com>
2024-01-06 21:17:01 -0300

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,13 @@
+# TutzGPT com React
+
+[Ver App](https://chatgpt-clone-rodrigotutz.vercel.app/)
+
+Instalar Dependências
+```bash
+    npm install
+```
+
+Iniciar Servidor
+```bash
+    npm run dev
+```
---
+++ src/components/ChatPlaceholder.tsx
@@ -0,0 +1,9 @@
+export const ChatPlaceholder = () => {
+    return (
+        <div className="m-5">
+            <h3 className="text-4xl font-bold text-center my-8">TutzGPT</h3>
+
+            <div></div>
+        </div>
+    )
+}
