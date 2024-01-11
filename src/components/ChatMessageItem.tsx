115566550550d67c501e989f5a3b7a4efb7368b3
Rodrigo Tutz <rodrigoantunestutz@gmail.com>
2024-01-11 00:17:06 -0300

    Leitura de markdwon

--- src/components/ChatMessageItem.tsx
+++ src/components/ChatMessageItem.tsx
@@ -2,9 +2,8 @@ import { ChatMessage } from "@/types/ChatMessage"
 import IconUser from "./icons/IconUser"
 import IconRobot from "./icons/IconRobot"
 import ReactMarkdown from 'react-markdown'
-import remarkGfm from "remark-gfm"
 import { Prism as SyntaxHighLighter } from 'react-syntax-highlighter' 
-import { twilight } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 type Props = {
     item: ChatMessage
@@ -24,7 +23,7 @@ export const ChatMessageItem = ({item}: Props) => {
                 </div>
 
                 <div className="flex-1 text-base whitespace-pre-wrap">
-                    <ReactMarkdown children={response} remarkPlugins={remarkGfm} 
+                    <ReactMarkdown children={response}  
                     components={{
                         code(props) {
                           const {children, className, node, ...rest} = props
@@ -35,7 +34,7 @@ export const ChatMessageItem = ({item}: Props) => {
                               PreTag="div"
                               children={String(children).replace(/\n$/, '')}
                               language={match[1]}
-                              style={twilight}
+                              style={darcula}
                             />
                           ) : (
                             <code {...rest} className={className}>
