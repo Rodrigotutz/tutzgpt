69f5856d8f2323bbd15c8055d31773b0d9c882e6
Rodrigo Tutz <rodrigoantunestutz@gmail.com>
2024-01-11 00:06:10 -0300

    Leitura de markdwon

--- src/components/ChatMessageItem.tsx
+++ src/components/ChatMessageItem.tsx
@@ -4,7 +4,7 @@ import IconRobot from "./icons/IconRobot"
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from "remark-gfm"
 import { Prism as SyntaxHighLighter } from 'react-syntax-highlighter' 
-import {nightOwl} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { nightOwl } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 type Props = {
     item: ChatMessage
